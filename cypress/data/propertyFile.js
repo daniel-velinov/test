ba790bd01e8f9032f2788e35ba8a3564b240add7
Daniel <daniel.velinov86@gmail.com>
2020-11-23 13:53:00 +0200

    added jas

---
+++ cypress/data/propertyFile.js
@@ -0,0 +1,92 @@
+'use strict';
+module.exports = {
+    "environment": "https://omnium.braingroup.ch/e2etest3/", 
+    "screenW" : 1680,
+    "screenH" : 1050,
+    "users": [
+        {
+            "name": "abandi-1",
+            "pass": "ptor",
+            "role": "Superuser",
+            "customer": [{
+                "name": "SINGER",
+                "modules": ["FINA", "FINB", "TAX", "PEPL", "HHB", "ANL"]
+            }]
+        } ,
+        {
+            "name": "abandi-2",
+            "pass": "ptor",
+            "role": "Superuser",
+            "customer": [{
+                "name": "GERBER",
+                "modules": ["FINA", "FINB", "TAX", "PEPL", "HHB", "ANL"]
+            }]
+        }, 
+        {
+            "name": "abandi-3",
+            "pass": "ptor",
+            "role": "Superuser",
+            "customer": [{
+                "name": "ROTT",
+                "modules": ["FINA", "FINB", "TAX", "PEPL", "HHB", "ANL"]
+            }]
+        },
+         {
+            "name": "abandi-4",
+            "pass": "ptor",
+            "role": "Superuser",
+            "customer": [{
+                "name": "SCHAR",
+                "modules": ["FINA", "FINB", "TAX", "PEPL", "HHB", "ANL"]
+            }]
+        }, 
+        {
+            "name": "abandi-5",
+            "pass": "ptor",
+            "role": "Superuser",
+            "customer": [{
+                "name": "ROUGE",
+                "modules": ["FINA", "FINB", "TAX", "PEPL", "HHB", "ANL"]
+            }]
+        }, 
+        {
+            "name": "abandi-6",
+            "pass": "ptor",
+            "role": "Superuser",
+            "customer": [{
+                "name": "KRATZ",
+                "modules": ["FINA", "FINB", "TAX", "PEPL", "HHB", "ANL"]
+            }]
+        }, 
+        {
+            "name": "abandi-7",
+            "pass": "ptor",
+            "role": "Superuser",
+            "customer": [{
+                "name": "HOLENSTEIN",
+                "modules": ["FINA", "FINB", "TAX", "PEPL", "HHB", "ANL"]
+            }]
+        },
+         {
+            "name": "abandi-8",
+            "pass": "ptor",
+            "role": "Superuser",
+            "customer": [{
+                "name": "ULBER",
+                "modules": ["FINA", "FINB", "TAX", "PEPL", "HHB", "ANL"]
+            }]
+        }, 
+        {
+            "name": "abandi-9",
+            "pass": "ptor",
+            "role": "Superuser",
+            "customer": [{
+                "name": "MACINTOSH",
+                "modules": ["FINA", "FINB", "TAX", "PEPL", "HHB", "ANL"]
+            }]
+        }
+    ]
+    
+}
+
+
